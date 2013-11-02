463939609f119f12964aeee10e5995cf1e579997
Sebastian Borrazas <seba.borrazas@gmail.com>
2013-11-02 21:09:26 -0200

    WIP

---
+++ initialize_script.sh
@@ -0,0 +1,11 @@
+# Install Git
+# TODO
+
+# Clone repos
+mkdir ~/dev_repos
+# TODO Iterate through each repo and clone it
+
+# Emacs configuration
+ln -s /Users/sebastian/dev_repos/dotfiles/emacs/ ~/.emacs.d
+ln -s /Users/sebastian/.bash_profile /Users/sebastian/.emacs_bash
+ln -s git/gitconfig.symlink ${current dir??}/.gitconfig
