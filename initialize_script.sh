425b39093bdb93cc98366da9e4def821bac907f3
Sebastian Borrazas <seba.borrazas@gmail.com>
2013-11-16 12:52:05 -0200

    WIP 4

--- initialize_script.sh
+++ initialize_script.sh
@@ -6,13 +6,16 @@ mkdir ~/dev_repos
 # TODO Ask for a list of repos, iterate through each and clone it
 
 # Emacs configuration
-ln -s /Users/sebastian/dev_repos/dotfiles/emacs/ ~/.emacs.d
-ln -s /Users/sebastian/.bash_profile /Users/sebastian/.emacs_bash
-ln -s git/gitconfig.symlink ${current dir??}/.gitconfig
+ln -s $HOME/dev_repos/dotfiles/emacs/ ~/.emacs.d
+ln -s $HOME/.bash_profile $HOME/.emacs_bash
+ln -s "$(pwd)/git/gitconfig.symlink" $HOME/.gitconfig
 
 # Establish emacs binary as the Emacs.app binary
 sudo mv /usr/bin/{,old}emacs
-sudo cp ./bin/emacs /usr/bin/emacs
+sudo ln -s "$(pwd)/bin/emacs" /usr/bin/emacs
+
+sudo mv /usr/bin/{,old}emacsclient
+sudo ln -s "$(pwd)/bin/emacsclient" /usr/bin/emacsclient
 
 # Node Version Manager (NVM)
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
