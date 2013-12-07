1e7d40771d44879b30312035ed8248031230ea0c
Sebastian Borrazas <seba.borrazas@gmail.com>
2013-12-07 13:21:55 -0200

    WIP 2

--- initialize_script.sh
+++ initialize_script.sh
@@ -12,6 +12,8 @@ mkdir ~/dev_repos
 ln -s $HOME/dev_repos/dotfiles/emacs/ ~/.emacs.d
 ln -s $HOME/.bash_profile $HOME/.emacs_bash
 ln -s "$(pwd)/git/gitconfig.symlink" $HOME/.gitconfig
+ln -s "$(pwd)/osx/bashrc.symlink" $HOME/.bashrc
+ln -s "$(pwd)/osx/bash_profile.symlink" $HOME/.bash_profile
 
 # Establish emacs binary as the Emacs.app binary
 sudo mv /usr/bin/{,old}emacs
@@ -21,11 +23,20 @@ sudo mv /usr/bin/{,old}emacsclient
 sudo ln -s "$(pwd)/bin/emacsclient" /usr/bin/emacsclient
 
 # Node Version Manager (NVM)
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
-# TODO: Install some node version inside nvm and also install something to uglify
-# https://github.com/creationix/nvm/
-
+brew install node
 # Install brew
 # ..
-# Install postgresql with brew
+
+# Brew - postgresql
 brew install postgresql
+# Initialize the location where postgres will store its data
+initdb /usr/local/var/postgres
+# Start the postgres server in the background
+pg_ctl -D /usr/local/var/pg_data -l /tmp/postgres_logfile start
+# Copy the launch agent to start postgres on login
+cp /usr/local/Cellar/postgresql/9.3.1/homebrew.mxcl.postgresql.plist ~/Library/LaunchAgents/
+launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
+
+# Python
+brew install python
+pip install virtualenv
